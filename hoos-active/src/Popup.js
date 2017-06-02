0a20c56df545b5d658ff3048799c99ee606db6f7
rfskinsfan <vineethgaddam@gmail.com>
2017-06-02 11:30:20 -0400

    finishedCss

--- hoos-active/src/Popup.js
+++ hoos-active/src/Popup.js
@@ -4,18 +4,21 @@ import ReactDOM from 'react-dom';
 export default class Popup extends React.Component{
     constructor(props){
     super(props);
+}
 
+addPeopleGoing(){
+    this.props.addPeopleGoing();
 }
 
 render(){
     return(
         <div id="vertical-menu">
             <a href="#" className="menu">Game Details</a>
-            <a href="#">Sport : {this.props.event}</a>
-            <a href="#"> Time: {this.props.time} </a>
-            <a href="#">Creator: {this.props.creator}</a>
-            <a href="#">Place: {this.props.placeName}</a>
-            <a href ="#"> Going?</a>
+            <a href="#" className="styleSubs">Sport : {this.props.event}</a>
+            <a href="#" className="styleSubs"> Time: {this.props.time} </a>
+            <a href="#" className="styleSubs">Creator: {this.props.creator}</a>
+            <a href="#" className="styleSubs">Place: {this.props.placeName}</a>
+            <a href ="#"  className="styleSubs" onClick={() => {this.addPeopleGoing()}}> Going?</a>
             {/*<a href="#"> People Going </a>*/}
         </div>
     )
